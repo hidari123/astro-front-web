3bc61bc110aa60f2940ef0eeb6c2685073554856
hidari123 <60548379+hidari123@users.noreply.github.com>
2024-01-18 13:52:20 +0800

    init

---
+++ pages/test.tsx
@@ -0,0 +1,12 @@
+/*
+ * @Author: hidari123
+ * @Date: 2024-01-18 11:05:26
+ * @LastEditors: hidari123
+ * @LastEditTime: 2024-01-18 11:11:28
+ * Copyright (c) 2024 by hidari, All Rights Reserved.
+ */
+const Test = () => {
+  return <div>about</div>;
+};
+
+export default Test;
